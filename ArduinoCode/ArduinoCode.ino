1aa1cb3e67fb1f07e7a9f36a5d9ee1ff30362268
RubenKindt <rubenKindt@live.be>
2020-11-19 09:58:43 +0100

    database for temp and sleep sec done

--- ArduinoCode/ArduinoCode.ino
+++ ArduinoCode/ArduinoCode.ino
@@ -1,26 +1,62 @@
+// .\ArduinoSketchUploader.exe --file= SorTeS_GW07.hex --port=COM74 --model=Leonardo
+// TODO: ctrl F for 'TODO'
+
 #include <Arduino_FreeRTOS.h>
 #include <EEPROM.h> //to bel able to writ in EEPROM using EEPROM.write(addr, val); our EEPROM is 1024 bits big
-
 #include <LoRa.h>
 
+#define SCK     15
+#define MISO    14
+#define MOSI    16
+#define SS      8
+#define RST     4
+#define DI0     7
+#define BAND    869300000
+#define PABOOST true 
+
+int sleepEEPROMLocation=0;
+int nextEntryAdressLocation=8;
+int firstdbEntry=nextEntryAdressLocation+8;
+
 // define tasks
-void TaskBlink( void *pvParameters );
-void TaskAnalogRead( void *pvParameters );
+//void TaskBlink( void *pvParameters );
+//void TaskAnalogRead( void *pvParameters );
 
-
-
+//nextEntryAdress
+byte nextEntryAdress;
+bool listeningToCommands;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
+  //EEPROM.write(nextEntryAdressLocation,byte(firstdbEntry));
   
-  // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
+  while (!Serial);
   
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB, on LEONARDO, MICRO, YUN, and other 32u4 based boards.
-  }
+  listeningToCommands=true;
+  
+  //DB SETUP
+  EEPROM.write(sleepEEPROMLocation,byte(10)); //10 sec
+  nextEntryAdress=EEPROM.read(nextEntryAdressLocation);
 
+
+  //INTERUPTS SETUP
+  interrupts();
+
+
+  
+  //LORA setup
+  Serial.println("LoRa Receiver");
+  LoRa.setPins(SS,RST,DI0);
+  if (!LoRa.begin(BAND,PABOOST )) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+  LoRa.onReceive(onReceive);
+  LoRa.receive();
+  
   // Now set up two tasks to run independently.
+  /*
   xTaskCreate(
     TaskBlink
     ,  "Blink"   // A name just for humans
@@ -38,44 +74,153 @@ void setup() {
     ,  NULL );
 
   // Now the task scheduler, which takes over control of scheduling individual tasks, is automatically started.
+  */
 }
 
-void loop()
-{
-  // Empty. Things are done in Tasks.
+
+void loop() {
+  //empty, we use Tasks
+  
+  if (listeningToCommands){
+    int command=0;
+     if (Serial.available() > 0) {
+        // read the incoming byte:
+        command=Serial.parseInt();
+        switch (command) {
+          case 1:
+            printLastEntry();
+            break;
+          case 2:
+            printdb();
+            break;
+          case 3:
+            //deepSleep();
+            break;
+          default:
+            break;
+        }
+      }
+    }
+}
+
+
+
+void onReceive (int packetSize){
+  if (packetSize == 0) return;          // if there's no packet, return
+
+  listeningToCommands=false;
+
+  String gateway="";
+  for (int i=0;i<4;i++) {            // can't use readString() in callback, so
+    gateway += (char)LoRa.read();      // add bytes one by one
+  }
+  Serial.print("Gateway: " );
+  Serial.println(gateway);
+  
+  if (gateway!="GW07"){
+    //Serial.println("wrong Gateway");
+    return;
+  }
+  
+  int8_t sec;                 // payload of packet
+  char charr="";
+  charr=(char)LoRa.read();
+  
+  sec = charr - '0';      
+
+  Serial.println("sleep sec: " + String(sec));
+  Serial.println("RSSI: " + String(LoRa.packetRssi()));
+  //Serial.println("Snr: " + String(LoRa.packetSnr()));
+
+  // send ack with temp
+  LoRa.beginPacket(); 
+  int8_t temp =getTemperatureInternal(); 
+  LoRa.write(temp);                     
+  LoRa.endPacket();
+  Serial.print("Temp: ");
+  Serial.println(temp);
+  writeTempSleepEEPROM(temp,sec);
+  Serial.println("-------------");
+  LoRa.receive();
+}
+
+
+
+void writeTempSleepEEPROM(int8_t temp,int8_t sleepSec){
+  Serial.println("---------------------------");
+  Serial.print("Inputs: ");
+  Serial.print(temp);
+  Serial.print(" ");
+  Serial.println(sleepSec);
+  Serial.println("---------------------------");
+
+
+  EEPROM.write(sleepEEPROMLocation,sleepSec);
+  nextEntryAdress=EEPROM.read(nextEntryAdressLocation);
+  EEPROM.write(nextEntryAdress,temp);
+  
+  Serial.print("writen temp at adress: ");
+  Serial.println(nextEntryAdress,DEC);
+
+  Serial.print("writen temp : ");
+  Serial.println(temp,BIN);
+  
+  nextEntryAdress+=8; //move nextEntry 
+  EEPROM.write(nextEntryAdressLocation,nextEntryAdress);
+  
+}
+void printLastEntry(){
+  Serial.println("printLastEntry: ");
+  nextEntryAdress=EEPROM.read(nextEntryAdressLocation);
+  byte adressLastEntry=nextEntryAdress-8;
+  if (adressLastEntry<firstdbEntry){
+    Serial.println("No entries");
+    return;
+  }
+  //Serial.print("at adress: ");
+  //Serial.println(adressLastEntry);
+  Serial.print("last Recorded temp: ");
+  Serial.println(EEPROM.read(adressLastEntry),DEC);
+  
+}
+
+void printdb(){
+  nextEntryAdress=EEPROM.read(nextEntryAdressLocation);
+  Serial.println("Printing DB");
+
+  if (nextEntryAdress<=firstdbEntry){
+    Serial.println("No entries");
+    return;
+  }
+  
+  /*
+  Serial.print("start at adress ");
+  Serial.print(firstdbEntry);
+  Serial.print(" - ");
+  Serial.print("stop at adress ");
+  Serial.println(nextEntryAdress-8);
+  */
+  
+  int entry = 1;
+  for (byte i=byte(firstdbEntry); i<nextEntryAdress; i+=8){
+    Serial.print("entry ");
+    Serial.print(entry);
+    Serial.print(": ");
+    Serial.println(i);
+    Serial.print(EEPROM.read(i),DEC);
+    Serial.println("°c");
+    Serial.println();
+  }
+
 }
 
 /*--------------------------------------------------*/
 /*---------------------- Tasks ---------------------*/
 /*--------------------------------------------------*/
-
 void TaskBlink(void *pvParameters)  // This is a task.
 {
   (void) pvParameters;
 
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, LEONARDO, MEGA, and ZERO 
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN takes care 
-  of use the correct LED pin whatever is the board used.
-  
-  The MICRO does not have a LED_BUILTIN available. For the MICRO board please substitute
-  the LED_BUILTIN definition with either LED_BUILTIN_RX or LED_BUILTIN_TX.
-  e.g. pinMode(LED_BUILTIN_RX, OUTPUT); etc.
-  
-  If you want to know what pin the on-board LED is connected to on your Arduino model, check
-  the Technical Specs of your board  at https://www.arduino.cc/en/Main/Products
-  
-  This example code is in the public domain.
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-*/
 
   // initialize digital LED_BUILTIN on pin 13 as an output.
   pinMode(LED_BUILTIN, OUTPUT);
@@ -93,15 +238,6 @@ void TaskAnalogRead(void *pvParameters)  // This is a task.
 {
   (void) pvParameters;
   
-/*
-  AnalogReadSerial
-  Reads an analog input on pin 0, prints the result to the serial monitor.
-  Graphical representation is available using serial plotter (Tools > Serial Plotter menu)
-  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
-
-  This example code is in the public domain.
-*/
-
   for (;;)
   {
     // read the input on analog pin 0:
@@ -110,4 +246,40 @@ void TaskAnalogRead(void *pvParameters)  // This is a task.
     Serial.println(sensorValue);
     vTaskDelay(1);  // one tick delay (15ms) in between reads for stability
   }
+}
+
+
+//function modified from
+//https://www.avrfreaks.net/forum/teensy-atmega32u4-chip-temperature-sensor-0
+int8_t getTemperatureInternal() {
+
+  //The temperature sensor and its internal driver are enabled when ADMUX value selects the temperature sensor as ADC input. The propagation delay of this driver is approximately 2μS. 
+  //Therefore two successive conversions are required. The correct temperature measurement will be the second one.  
+  //source http://ww1.microchip.com/downloads/en/devicedoc/atmel-7766-8-bit-avr-atmega16u4-32u4_datasheet.pdf
+  //ADMUX value selects the temperature sensor as ADC input, 100111
+  //Wrong: ADMUX = (1<<REFS1) | (1<<REFS0) | (1<<MUX2) | (1<<MUX1) | (1<<MUX0);
+  //source see page 314 of http://ww1.microchip.com/downloads/en/devicedoc/atmel-7766-8-bit-avr-atmega16u4-32u4_datasheet.pdf
+  ADMUX = (1<<REFS1) | (1<<MUX2) | (1<<MUX1) | (1<<MUX0);
+  ADCSRB |= (1<<MUX5);
+
+  //delay(2) onacceptable TODO
+ 
+  // start the conversion
+  ADCSRA |= bit(ADSC);
+
+  // ADSC is cleared when the conversion finishes
+  while (ADCSRA & bit(ADSC));
+
+  uint8_t low  = ADCL;
+  uint8_t high = ADCH;
+
+  //discard first reading
+  ADCSRA |= bit(ADSC);
+  while (ADCSRA & bit(ADSC));
+  low  = ADCL;
+  high = ADCH;
+  int a = (high << 8) | low;
+
+  //return temperature in C
+  return a - (272+3); // +3 is calibration
 }
